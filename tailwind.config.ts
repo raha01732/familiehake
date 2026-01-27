faa3c96dea807c24c4d8fbdf628db8804b64ee56
Ralf Hake <ralfhake01@gmail.com>
2026-01-27 14:08:42 +0100

    updated files to bugfix layout problems

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,12 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}"
-  ],
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
+],
+
   theme: {
   	extend: {
   		borderRadius: {
