1ce39afef84d67cb037cf831ab22b0793a9cb0e8
Ralf Hake <ralfhake01@gmail.com>
2026-01-27 19:18:56 +0100

    correction of ;

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,7 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import posthog from 'posthog-js',
+import posthog from 'posthog-js';
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
